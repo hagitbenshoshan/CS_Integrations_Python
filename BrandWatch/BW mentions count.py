58d07710d27e836e6250ee2b4af6acfd226da0a0
Jonathan Goorin <jonathan@cooladata.com>
2019-01-31 14:46:43 +0200

    Brand watch fix

--- BrandWatch/BW mentions count.py
+++ BrandWatch/BW mentions count.py
@@ -1,24 +1,25 @@
 # Brings data from Brandwatch’s api and counts the number of mentions.
-
 import logging
 import requests
 from datetime import datetime, date, time, timedelta
 import json
 import pandas as pd
 
-address = 'https://newapi.brandwatch.com/oauth/token?username=hagit@cooladata.com&grant_type=api-password&client_id=brandwatch-api-client'
-data = {'password':'<Insert Password here>'}
+logger = logging.getLogger("bwapi")
+
+USER_NAME = 'your_username@example.com'
+PASSWORD = 'your_password'
+
+address = 'https://newapi.brandwatch.com/oauth/token?username=%s' \
+          '&grant_type=api-password&client_id=brandwatch-api-client' % USER_NAME
+
+data = {'password': PASSWORD}
+
 r = requests.post(address, data=data)
-a = r.content
-jsonToPython = json.loads(a)
+jsonToPython = json.loads(r.content)
 my_access_token = jsonToPython['access_token']
 token = my_access_token
 
-logger = logging.getLogger("bwapi")
-
-YOUR_ACCOUNT = '<Insert account mail>'
-YOUR_PASSWORD = '<Insert account Password>'
-YOUR_PROJECT = '<Insert project ID>'
 
 url = 'https://newapi.brandwatch.com/projects/<Insert Project number here>'
 headers = {'Authorization': 'bearer %s' % my_access_token}
@@ -40,15 +41,15 @@ midnight = datetime.combine(date.today(), time.min)
 yesterday_midnight = midnight - timedelta(days=1)
 fromdate = str(yesterday_midnight)[:10] + 'T00%3A00%3A00.000Z'
 todate = str(midnight)[:10] + 'T00%3A00%3A00.000Z'
+
 # Queries
 url = 'https://newapi.brandwatch.com/projects/1998169013/data/mentions/count?endDate=' + todate + '&queryId%5B%5D=1998336099&startDate=' + fromdate
+
 # print url
 headers = {'Authorization': 'bearer %s' % my_access_token}  # ACR FACEBOOK
 response = requests.get(url=url, headers=headers)
 
 d = json.loads(response.text)
-# print d
-
 
 if response.ok:
     mentionsCount = str(d["mentionsCount"])
